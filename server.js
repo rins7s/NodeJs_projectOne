b85afe10e3655758eaa735dd7102b2d47ed31161
rins rajan <rins.rajan7@gmail.com>
2017-08-29 11:31:16 +0530

    node js web server
    creating basic webserver with nodejs

--- server.js
+++ server.js
@@ -12,4 +12,36 @@ var mimetypes = {
     "png": "image/png",
     "js": "text/js",
     "css": "text/css",
-}
+};
+
+//create server
+http.createServer(function(req, res) {
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log('Loading ' + uri);
+    var stats;
+
+    try {
+        stats = fs.lstatSync(fileName);
+    } catch (e) {
+        res.writeHead(404, { 'Content-type': 'text/plain' });
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+
+    if (stats.isFile()) {
+        var mimetype = mimetypes[path.extname(fileName).split(".").reverse()[0]];
+        res.writeHead(200, { 'Content-Type': mimetype });
+
+        var fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    } else if (stats.isDirectory()) {
+        res.writeHead(302, { 'Location': 'index.html' });
+        res.end();
+    } else {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write('500 Internal Error\n');
+        res.end();
+    }
+}).listen(3000);
